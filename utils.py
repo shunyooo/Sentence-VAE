bd4db03f1f26d0483858eec8a473f3eb93959162
Shunyo Kawamoto <synyo1028@yahoo.co.jp>
2019-11-20 06:32:18 +0000

    add notebook for train japanese

--- utils.py
+++ utils.py
@@ -3,6 +3,34 @@ import numpy as np
 from torch.autograd import Variable
 from collections import defaultdict, Counter, OrderedDict
 
+class AttributeDict(object):
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __getstate__(self):
+        return self.obj.items()
+
+    def __setstate__(self, items):
+        if not hasattr(self, 'obj'):
+            self.obj = {}
+        for key, val in items:
+            self.obj[key] = val
+
+    def __getattr__(self, name):
+        if name in self.obj:
+            return self.obj.get(name)
+        else:
+            return None
+
+    def fields(self):
+        return self.obj
+
+    def keys(self):
+        return self.obj.keys()
+    
+    def __repr__(self):
+        return f'<AttrDict{self.obj}>'
+
 class OrderedCounter(Counter, OrderedDict):
     'Counter that remembers the order elements are first encountered'
 
