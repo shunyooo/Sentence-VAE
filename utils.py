30a15f9c19e89c90cc4352c94b3766c326d79475
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2019-11-26 20:47:58 +0900

    update experiment_name can input

--- utils.py
+++ utils.py
@@ -76,18 +76,21 @@ def interpolate(start, end, steps):
 def expierment_name(args, ts):
 
     exp_name = str()
-    exp_name += "BS=%i_"%args.batch_size
-    exp_name += "LR={}_".format(args.learning_rate)
-    exp_name += "EB=%i_"%args.embedding_size
-    exp_name += "%s_"%args.rnn_type.upper()
-    exp_name += "HS=%i_"%args.hidden_size
-    exp_name += "L=%i_"%args.num_layers
-    exp_name += "BI=%i_"%args.bidirectional
-    exp_name += "LS=%i_"%args.latent_size
-    exp_name += "WD={}_".format(args.word_dropout)
-    exp_name += "ANN=%s_"%args.anneal_function.upper()
-    exp_name += "K={}_".format(args.k)
-    exp_name += "X0=%i_"%args.x0
+    if args.expierment_name is not None:
+        exp_name += f'{args.expierment_name}_'
+    else:
+        exp_name += "BS=%i_"%args.batch_size
+        exp_name += "LR={}_".format(args.learning_rate)
+        exp_name += "EB=%i_"%args.embedding_size
+        exp_name += "%s_"%args.rnn_type.upper()
+        exp_name += "HS=%i_"%args.hidden_size
+        exp_name += "L=%i_"%args.num_layers
+        exp_name += "BI=%i_"%args.bidirectional
+        exp_name += "LS=%i_"%args.latent_size
+        exp_name += "WD={}_".format(args.word_dropout)
+        exp_name += "ANN=%s_"%args.anneal_function.upper()
+        exp_name += "K={}_".format(args.k)
+        exp_name += "X0=%i_"%args.x0
     exp_name += "TS=%s"%ts
 
     return exp_name
