235ed07a24bfa09d6d262b6564fcd2d3967bd664
Shunyo Kawamoto <synyo1028@yahoo.co.jp>
2019-12-20 12:30:26 +0000

    fix

--- utils.py
+++ utils.py
@@ -73,24 +73,24 @@ def interpolate(start, end, steps):
 
     return interpolation.T
 
-def expierment_name(args, ts):
+def experiment_name(args, ts):
 
     exp_name = str()
-    if args.expierment_name is not None:
-        exp_name += f'{args.expierment_name}_'
-    else:
-        exp_name += "BS=%i_"%args.batch_size
-        exp_name += "LR={}_".format(args.learning_rate)
-        exp_name += "EB=%i_"%args.embedding_size
-        exp_name += "%s_"%args.rnn_type.upper()
-        exp_name += "HS=%i_"%args.hidden_size
-        exp_name += "L=%i_"%args.num_layers
-        exp_name += "BI=%i_"%args.bidirectional
-        exp_name += "LS=%i_"%args.latent_size
-        exp_name += "WD={}_".format(args.word_dropout)
-        exp_name += "ANN=%s_"%args.anneal_function.upper()
-        exp_name += "K={}_".format(args.k)
-        exp_name += "X0=%i_"%args.x0
+    if args.experiment_name is not None:
+        exp_name += f'{args.experiment_name}_'
+        
+    exp_name += "BS=%i_"%args.batch_size
+    exp_name += "LR={}_".format(args.learning_rate)
+    exp_name += "EB=%i_"%args.embedding_size
+    exp_name += "%s_"%args.rnn_type.upper()
+    exp_name += "HS=%i_"%args.hidden_size
+    exp_name += "L=%i_"%args.num_layers
+    exp_name += "BI=%i_"%args.bidirectional
+    exp_name += "LS=%i_"%args.latent_size
+    exp_name += "WD={}_".format(args.word_dropout)
+    exp_name += "ANN=%s_"%args.anneal_function.upper()
+    exp_name += "K={}_".format(args.k)
+    exp_name += "X0=%i_"%args.x0
     exp_name += "TS=%s"%ts
-
+    
     return exp_name
